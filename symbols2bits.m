71d0a12292cc669c8ab18040d296455f4123e198
Tpetren <tpetren@gmail.com>
2015-09-18 16:30:06 +0200

    Update symbols2bits.m

--- symbols2bits.m
+++ symbols2bits.m
@@ -2,8 +2,27 @@ function y = symbols2bits(symbols)
 %symbols2bits Does frame synchronization and converts symbols to bits.
 %   symbols - The sequence of symbols.
 
-y = 0;
+[m,n]=size(symbols);
+bits=[];
 
+% An if statement makes sure that the symbols can be in the columns or rows
+% in the symbol matrix
+
+if n>m
+    for i=1:n
+        for j=1:m
+            bits = [bits symbols(j,i)];
+        end
+    end
+else
+    for k=1:m
+        for l=1:n
+            bits = [bits symbols(k,l)];
+        end
+    end
+end
+
+y = bits;
 
 end
 
